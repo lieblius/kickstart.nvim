24b501133402df1d484098ec2d95b159d1ce1ae5
Kyle Liebler <kyle@airspace-intelligence.com>
2025-05-01 14:06:12 -0400

    refactor: improve harpoon configuration and add telescope integration

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,43 +1,78 @@
 return {
-  'ThePrimeagen/harpoon',
-  branch = 'harpoon2',
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
   dependencies = {
-    'nvim-lua/plenary.nvim',
+    "n--vim.lua/plenary.nvim",
+    "n--vim.telescope/telescope.nvim",
   },
   config = function()
-    local harpoon = require 'harpoon'
+    local harpoon = require("harpoon")
 
     -- REQUIRED
     harpoon:setup()
 
-    -- Basic keymaps
-    vim.keymap.set('n', '<leader>a', function()
+    -- Helper functions
+
+    local add_file = function()
       harpoon:list():append()
-    end, { desc = 'Harpoon: Add file' })
-    vim.keymap.set('n', '<leader>h', function()
+    end
+    local toggle_menu = function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
-    end, { desc = 'Harpoon: Toggle menu' })
+    end
+    local goto_file = function(i)
+      return function()
+        harpoon:list():select(i)
+      end
+    end
+    local goto_prev = function()
+      harpoon:list():prev()
+    end
+    local goto_next = function()
+      harpoon:list():next()
+    end
+
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers")
+        .new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table({
+            results = file_paths,
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        })
+        :find()
+    end
+
+    local open_telescope = function()
+      toggle_telescope(harpoon:list())
+    end
+
+    -- Basic keymaps
+
+    vim.keymap.set("n", "<leader>ma", add_file, { desc = "Harpoon: Add file" })
+    vim.keymap.set("n", "<leader>mh", toggle_menu, { desc = "Harpoon: Toggle menu" })
 
     -- Navigation
-    vim.keymap.set('n', '<leader>1', function()
-      harpoon:list():select(1)
-    end, { desc = 'Harpoon: File 1' })
-    vim.keymap.set('n', '<leader>2', function()
-      harpoon:list():select(2)
-    end, { desc = 'Harpoon: File 2' })
-    vim.keymap.set('n', '<leader>3', function()
-      harpoon:list():select(3)
-    end, { desc = 'Harpoon: File 3' })
-    vim.keymap.set('n', '<leader>4', function()
-      harpoon:list():select(4)
-    end, { desc = 'Harpoon: File 4' })
+
+    vim.keymap.set("n", "<leader>1", goto_file(1), { desc = "Harpoon: File 1" })
+    vim.keymap.set("n", "<leader>2", goto_file(2), { desc = "Harpoon: File 2" })
+    vim.keymap.set("n", "<leader>3", goto_file(3), { desc = "Harpoon: File 3" })
+    vim.keymap.set("n", "<leader>4", goto_file(4), { desc = "Harpoon: File 4" })
 
     -- Navigate prev/next
-    vim.keymap.set('n', '<leader>p', function()
-      harpoon:list():prev()
-    end, { desc = 'Harpoon: Prev file' })
-    vim.keymap.set('n', '<leader>n', function()
-      harpoon:list():next()
-    end, { desc = 'Harpoon: Next file' })
+
+    vim.keymap.set("n", "<leader>mp", goto_prev, { desc = "Harpoon: Prev file" })
+    vim.keymap.set("n", "<leader>mn", goto_next, { desc = "Harpoon: Next file" })
+
+    -- Telescope integration
+
+    vim.keymap.set("n", "<leader>mt", open_telescope, { desc = "Harpoon: Open in Telescope" })
   end,
 }
